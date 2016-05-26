586799963015411ae421682e37dbfa18dda091a4
Christopher Cote <chris@entropealabs.com>
2016-05-26 13:30:07 -0500

    adds nerves and movi support

--- lib/nodeponics.ex
+++ lib/nodeponics.ex
@@ -3,8 +3,14 @@ defmodule Nodeponics do
     require Logger
 
     @name __MODULE__
+    @interface "wlan0"
+    @ssid "CRT-Internal"#System.get_env("SSID")
+    @psk "CRTUnderscoreSpelledOut"#System.get_env("PSK")
+    @key_management :"WPA-PSK"
 
     def start(_type, _args) do
         Nodeponics.Supervisor.start_link()
+        Movi.add_handler(Nodeponics.Voice.Handler)
+        Nerves.InterimWiFi.setup(@interface, ssid: @ssid, key_mgmt: @key_management, psk: @psk)
     end
 end
